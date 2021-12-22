a006398fb8c468f884dc532f10241d54e3220e42
RyanDeLong <rdelong111@gmail.com>
2021-12-21 20:47:22 -0600

    commit tree 53bdd7a51c67eaf94dd885b363c00fc0c5f9dfbe

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// code your solution here
+function superbowlWin(yearlist) {
+	if (yearlist.find(x => x.result === 'W')) {
+		return yearlist.find(x => x.result === 'W').year;
+	}
+	else {
+		return undefined;
+	}
+}
